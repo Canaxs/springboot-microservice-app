ec74d013adc5d65746c8b3d08206317fbe3527f7
Meriç Cana <91698544+Canaxs@users.noreply.github.com>
2022-04-09 22:51:32 +0300

    security-classes-added

---
+++ post-service/src/main/java/com/microservice/postservice/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.microservice.postservice.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${service.security.username}")
+    private String SECURITY_USERNAME;
+
+    @Value("${service.security.password}")
+    private String SECURITY_PASSWORD;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(encoder)
+                .withUser(SECURITY_USERNAME)
+                .password(encoder.encode(SECURITY_PASSWORD))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
